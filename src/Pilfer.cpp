ad26eb7ef0878b2549a04f9904e00c7416a946aa
NullCodeMusic <wilsonryanb@gmail.com>
2025-10-24 23:51:39 -0400

    A Saw B - minor visual changes to panel background Pilfer - Algorithm done, polyphony added, panel laid out

--- src/Pilfer.cpp
+++ src/Pilfer.cpp
@@ -16,6 +16,7 @@ struct Pilfer : Module {
 		DRIVE_PARAM,
 		HFLF_BUTTON,
 		LINEXP_BUTTON,
+		RECTPOL_BUTTON,
 		PARAMS_LEN
 	};
 	enum InputId {
@@ -38,6 +39,8 @@ struct Pilfer : Module {
 	enum LightId {
 		VELOCITY_LIGHT,
 		BOUNCING_LIGHT,
+		D1_LIGHT,
+		D2_LIGHT,
 		LIGHTS_LEN
 	};
 
@@ -50,18 +53,20 @@ struct Pilfer : Module {
 		configParam(DRIVE_PARAM, 0.f, 5.f, 0.f, "Drive");
 
 		configSwitch(HFLF_BUTTON, 0.f,1.f,1.f,"Range",{"Low","High"});
-		configSwitch(LINEXP_BUTTON, 0.f,1.f,1.f,"Friction Mode",{"B","A"});
+		configSwitch(LINEXP_BUTTON, 0.f,1.f,1.f,"Friction",{"Linear","Exponential"});
+		configSwitch(RECTPOL_BUTTON, 0.f,1.f,1.f,"Coordinates",{"Rect","Polar"});
 		configInput(ACC_CV_INPUT, "Acceleration CV");
 		configInput(FRI_CV_INPUT, "Friction CV");
 		configInput(TOL_CV_INPUT, "Distance CV");
 		configInput(BOU_CV_INPUT, "Bounce CV");
-		configInput(AUDIO_X_INPUT, "Target X In");
-		configOutput(AUDIO_X_OUTPUT, "X Position Out");
-		configInput(AUDIO_Y_INPUT, "Target Y In");
-		configOutput(AUDIO_Y_OUTPUT, "Y Position Out");
+		configInput(AUDIO_X_INPUT, "Target X/R");
+		configOutput(AUDIO_X_OUTPUT, "X Position");
+		configInput(AUDIO_Y_INPUT, "Target Y/θ");
+		configOutput(AUDIO_Y_OUTPUT, "Y Position");
+		configOutput(VEL_OUTPUT, "Velocity");
 		configBypass(AUDIO_X_INPUT,AUDIO_X_OUTPUT);
 		configBypass(AUDIO_Y_INPUT,AUDIO_Y_OUTPUT);
-		configInput(SHAKE_CV_INPUT, "Shake In");
+		configInput(SHAKE_CV_INPUT, "Shake");
 		configInput(DRI_CV_INPUT, "Drive CV");
 
 		configParam(ACC_CV_PARAM, -1.f, 1.f, 0.f, "Acceleration CV");
@@ -72,161 +77,237 @@ struct Pilfer : Module {
 		configParam(SHAKE_CV_PARAM, -1.f, 1.f, 0.f, "Shake Amount");
 	}
 	
-	float pos_x = 0;
-	float pos_y = 0;
-	float vel_x = 0;
-	float vel_y = 0;
+	float pos_x[16] = {};
+	float pos_y[16] = {};
+	float vel_x[16] = {};
+	float vel_y[16] = {};
 	int mode = 0;
+	float paramsum=0;
+	std::vector<float> baseparams = {0,0,0,0,0};
+	std::vector<float> cvparams = {0,0,0,0,0};
+	float acceleration;
+	float friction;
+	float bounce;
+	float tolerance;
+	float drive;
+
+	bool filterParams(int idx){
+		bool updateparams;
+		//MAIN
+		float nextsum = params[ACCEL_PARAM].getValue() +
+		params[FRICT_PARAM].getValue() +
+		params[BOUNCE_PARAM].getValue() +
+		params[TOLERANCE_PARAM].getValue() +
+		params[DRIVE_PARAM].getValue();
+
+		if(abs(nextsum-paramsum)>=0.0001){
+			baseparams[0] = pow(10,params[ACCEL_PARAM].getValue());
+			baseparams[1] = pow(params[FRICT_PARAM].getValue(),4);
+			baseparams[2] = params[BOUNCE_PARAM].getValue();
+			baseparams[3] = params[TOLERANCE_PARAM].getValue();
+			baseparams[4] = params[DRIVE_PARAM].getValue();
+
+			updateparams = true;
+		}
+
+		//CVS
+		if(inputs[ACC_CV_INPUT].isConnected()){
+			cvparams[0] = dsp::exp2_taylor5(inputs[ACC_CV_INPUT].getVoltage(idx) * params[ACC_CV_PARAM].getValue());
+
+			updateparams = true;
+		}else{
+			cvparams[0] = 1;
+		}
+
+		if(inputs[FRI_CV_INPUT].isConnected()){
+			cvparams[1] = inputs[FRI_CV_INPUT].getVoltage(idx) * params[FRI_CV_PARAM].getValue() / 10;
+
+			updateparams = true;
+		}else{
+			cvparams[1] = 0;
+		}
+
+		if(inputs[BOU_CV_INPUT].isConnected()){
+			cvparams[2] = inputs[BOU_CV_INPUT].getVoltage(idx) * params[BOU_CV_PARAM].getValue() / 10;
+
+			updateparams = true;
+		}else{
+			cvparams[2] = 0;
+		}
+
+		if(inputs[TOL_CV_INPUT].isConnected()){
+			cvparams[3] = inputs[TOL_CV_INPUT].getVoltage(idx) * params[TOL_CV_PARAM].getValue() / 2;
+
+			updateparams = true;
+		}else{
+			cvparams[3] = 0;
+		}
+
+		if(inputs[DRI_CV_INPUT].isConnected()){
+			cvparams[4] = inputs[DRI_CV_INPUT].getVoltage(idx) * params[DRI_CV_PARAM].getValue() / 2;
+
+			updateparams = true;
+		}else{
+			cvparams[4] = 0;
+		}
+
+		return updateparams;
+	}
 
 	void process(const ProcessArgs& args) override {
 
 		float delta = args.sampleTime;
-
-		float acceleration = pow(10,params[ACCEL_PARAM].getValue()) * 
-			pow(2,inputs[ACC_CV_INPUT].getVoltage() * params[ACC_CV_PARAM].getValue());
-		float friction = clamp(	
-			pow(params[FRICT_PARAM].getValue(),4) + //Main
-			inputs[FRI_CV_INPUT].getVoltage() * params[FRI_CV_PARAM].getValue() / 10, //CV
-			0.f,1.f); //Min/Max
-		float bounce = clamp(	
-			params[BOUNCE_PARAM].getValue() + //Main
-			inputs[BOU_CV_INPUT].getVoltage() * params[BOU_CV_PARAM].getValue() / 10, //CV
-			0.f,1.f); //Min/Max
-		float tolerance = clamp(	
-			params[TOLERANCE_PARAM].getValue() + //Main
-			inputs[TOL_CV_INPUT].getVoltage() * params[TOL_CV_PARAM].getValue() / 2, //CV
-			0.f,10.f); //Min/Max
-		float drive = clamp(	
-			params[DRIVE_PARAM].getValue() + //Main
-			inputs[DRI_CV_INPUT].getVoltage() * params[DRI_CV_PARAM].getValue() / 2, //CV
-			0.f,10.f); //Min/Max
 		float hflf = params[HFLF_BUTTON].getValue();
-		float shake = inputs[SHAKE_CV_INPUT].getVoltage()*params[SHAKE_CV_PARAM].getValue();
+		float rectpol = params[RECTPOL_BUTTON].getValue();
 
-		
-		if(hflf<1){
-			acceleration/=100;
-			tolerance/=60;
-		}
+		int channels = std::max(std::max(inputs[AUDIO_X_INPUT].getChannels(),
+		inputs[AUDIO_Y_INPUT].getChannels()),
+		std::max(inputs[SHAKE_CV_INPUT].getChannels(),1));
 
-		switch (mode)
+		float outvel;
+
+		for(int i = 0;i<channels;i++)
 		{
-		case 1:
-				pilfer1d(delta,drive,tolerance,acceleration,friction,shake,bounce);
-				outputs[AUDIO_Y_OUTPUT].setVoltage(inputs[AUDIO_X_INPUT].getVoltage());
-			break;
-		
-		case 2:
-				pilfer2d(delta,drive,tolerance,acceleration,friction,shake,bounce);
-			break;
-		
-		default:
-				outputs[AUDIO_X_OUTPUT].setVoltage(0);
-				outputs[AUDIO_Y_OUTPUT].setVoltage(0);
-			break;
+			if(filterParams(i)){
+				acceleration = baseparams[0] * cvparams[0];
+				friction = clamp(baseparams[1] + cvparams[1], 0.f,1.f);
+				bounce = clamp(baseparams[2] + cvparams[2], 0.f,1.f); 
+				tolerance = clamp(baseparams[3] + cvparams[3], 0.f,10.f); 
+				drive = clamp(baseparams[4] + cvparams[4], 0.f,10.f);
+			}
+
+			float shake = inputs[SHAKE_CV_INPUT].getVoltage(i)*params[SHAKE_CV_PARAM].getValue();
+
+			
+			if(hflf<1){
+				acceleration/=100;
+				tolerance/=60;
+			}
+			
+			float x;
+			float y;
+			if(rectpol<1||mode==1){
+				x = inputs[AUDIO_X_INPUT].getVoltage(i)*(1+drive);
+				y = inputs[AUDIO_Y_INPUT].getVoltage(i)*(1+drive);
+			}else{
+				float r = inputs[AUDIO_X_INPUT].getVoltage(i)*(1+drive);
+				float t = inputs[AUDIO_Y_INPUT].getVoltage(i)*(1+drive);
+				x = cosf((t/5+0.25)*3.1415)*r;
+				y = sinf((t/5+0.25)*3.1415)*r;
+			}
+
+			switch (mode)
+			{
+			case 1:
+					pilfer1d(i,x,delta,drive,tolerance,acceleration,friction,shake,bounce);
+					outputs[AUDIO_Y_OUTPUT].setVoltage(0,i);
+				break;
+			
+			case 2:
+					pilfer2d(i,x,y,delta,drive,tolerance,acceleration,friction,shake,bounce);
+				break;
+
+			default:
+					outputs[AUDIO_X_OUTPUT].setVoltage(0,i);
+					outputs[AUDIO_Y_OUTPUT].setVoltage(0,i);
+				break;
+			}
+
+			outvel = magnitude(vel_x[i],vel_y[i]);
+			outputs[VEL_OUTPUT].setVoltage(outvel,i);
 		}
 
-		outputs[VEL_OUTPUT].setVoltage(magnitude(vel_x,vel_y));
-		lights[VELOCITY_LIGHT].setBrightness(magnitude(vel_x,vel_y));
+		outputs[AUDIO_X_OUTPUT].setChannels(channels);
+		outputs[AUDIO_Y_OUTPUT].setChannels(channels);
+		outputs[VEL_OUTPUT].setChannels(channels);
+
+		lights[BOUNCING_LIGHT].setBrightness(lights[BOUNCING_LIGHT].getBrightness()/1.0005f);
+		lights[VELOCITY_LIGHT].setBrightness(outvel);
 	}
 
 	template <typename T> int sign(T val) {
     	return (T(0) < val) - (val < T(0));
 	}
 
-	void pilfer1d(float delta, float drive, float tolerance, float acceleration, float friction, float shake, float bounce){
-		float audio = inputs[AUDIO_X_INPUT].getVoltage()*(1+drive);
+	void pilfer1d(int channel, float audio,float delta, float drive, float tolerance, float acceleration, float friction, float shake, float bounce){
 
 			//Will I move?
-			float diff = audio-pos_x;
+			float diff = audio-pos_x[channel];
 			float move = sign(diff)*(abs(diff)>tolerance)*acceleration*delta;
-			move += shake*acceleration*delta;
+			move += shake*acceleration*delta*0.25;
 			//Update Velocity
-			vel_x += move;
+			vel_x[channel] += move;
 
 			//Move
-			pos_x += vel_x;
+			pos_x[channel] += vel_x[channel];
 
 			//Bounce
-			if(abs(pos_x)>=10&&sign(vel_x)==sign(pos_x)){
-				lights[BOUNCING_LIGHT].setBrightness(1);
-				vel_x = -vel_x*bounce;
+			if(abs(pos_x[channel])>=10&&sign(vel_x[channel])==sign(pos_x[channel])){
+				lights[BOUNCING_LIGHT].setBrightness(bounce);
+				vel_x[channel] = -vel_x[channel]*bounce;
 			}
 			//Friction
 			if(params[LINEXP_BUTTON].getValue()<1){
-				vel_x-=sign(vel_x)*std::min(abs(vel_x),friction*acceleration*delta);
+				vel_x[channel]-=sign(vel_x[channel])*std::min(abs(vel_x[channel]),friction*acceleration*delta);
 			}else{
-				vel_x/=1+friction;
+				vel_x[channel]/=1+friction;
 			}
 
 			//Output
 			float out = 0;
-				out = pos_x/(1+drive);
+				out = pos_x[channel]/(1+drive);
 				out = tanh(out/12)*12;
 
-			outputs[AUDIO_X_OUTPUT].setVoltage(out);
-
-			//Decay Lights
-			lights[BOUNCING_LIGHT].setBrightness(lights[BOUNCING_LIGHT].getBrightness()/1.0005f);
+			outputs[AUDIO_X_OUTPUT].setVoltage(out, channel);
 	}
 
-	void pilfer2d(float delta, float drive, float tolerance, float acceleration, float friction, float shake, float bounce){
-		float aud_x = inputs[AUDIO_X_INPUT].getVoltage()*(1+drive);
-		float aud_y = inputs[AUDIO_Y_INPUT].getVoltage()*(1+drive);
+	void pilfer2d(int channel, float aud_x, float aud_y, float delta, float drive, float tolerance, float acceleration, float friction, float shake, float bounce){
 
 		//Will I move?
-		std::vector<float> diff = dir_dist(pos_x,pos_y,aud_x,aud_y);
-		float move = (diff[2]>tolerance)*acceleration*delta;
-		move += shake*acceleration*delta;
-		float dir_x = diff[0];
-		float dir_y = diff[1];
+		float dir_x = aud_x-pos_x[channel];
+		float dir_y = aud_y-pos_y[channel];
+		float diff = magnitude(dir_x,dir_y);
+		if(diff!=0){
+			dir_x/=diff;
+			dir_y/=diff;
+		}else{
+			dir_x = 0;
+			dir_y = 0;
+		}
+		float move = (diff>tolerance)*acceleration*delta;
+		move += shake*acceleration*delta*0.25; 
 
 		//Update velocity
-		vel_x += dir_x*move;
-
-		vel_y += dir_y*move;
+		vel_x[channel] += dir_x*move;
+		vel_y[channel] += dir_y*move;
 
 		//Move
-		pos_x += vel_x;
-		pos_y += vel_y;
+		pos_x[channel] += vel_x[channel];
+		pos_y[channel] += vel_y[channel];
 
 		//Bounce - Need to figure out algo so empty for now
-		if(magnitude(pos_x,pos_y)>=10){
-			lights[BOUNCING_LIGHT].setBrightness(1);
-			vel_x = -vel_x*bounce;
-			vel_y = -vel_y*bounce;
+		if(magnitude(pos_x[channel],pos_y[channel])>=10){
+			lights[BOUNCING_LIGHT].setBrightness(bounce);
+			vel_x[channel] = -vel_x[channel]*bounce;
+			vel_y[channel] = -vel_y[channel]*bounce;
 		}
 
 		//Friction
 		if(params[LINEXP_BUTTON].getValue()<1){
-			vel_x-=sign(vel_x)*std::min(abs(vel_x),friction*acceleration*delta);
-			vel_y-=sign(vel_y)*std::min(abs(vel_y),friction*acceleration*delta);
+			vel_x[channel]-=sign(vel_x[channel])*std::min(abs(vel_x[channel]),friction*acceleration*delta);
+			vel_y[channel]-=sign(vel_y[channel])*std::min(abs(vel_y[channel]),friction*acceleration*delta);
 		}else{
-			vel_x/=1+friction;
-			vel_y/=1+friction;
+			vel_x[channel]/=1+friction;
+			vel_y[channel]/=1+friction;
 		}
 
 		//Output
-		
-		outputs[AUDIO_X_OUTPUT].setVoltage(tanh(pos_x/12/(1+drive))*12);
-		outputs[AUDIO_Y_OUTPUT].setVoltage(tanh(pos_y/12/(1+drive))*12);
-
-		//Decay Lights
-		lights[BOUNCING_LIGHT].setBrightness(lights[BOUNCING_LIGHT].getBrightness()/1.0005f);
-	}
-
-	std::vector<float> dir_dist(float x1, float y1, float x2, float y2){
-		float x = x2 - x1;
-		float y = y2 - y1;
-		float mag = magnitude(x,y);
-		if(mag == 0){
-			return {0,0,0};
-		}
-		return {x/mag , y/mag, mag};
+		outputs[AUDIO_X_OUTPUT].setVoltage(tanh(pos_x[channel]/12/(1+drive))*12, channel);
+		outputs[AUDIO_Y_OUTPUT].setVoltage(tanh(pos_y[channel]/12/(1+drive))*12, channel);
 	}
 	
 	float magnitude(float x, float y){
-		float out = sqrt(pow(x,2)+pow(y,2));
+		float out = sqrt(x*x+y*y);
 		if(std::isnan(out)){
 			return 0;
 		}
@@ -234,15 +315,22 @@ struct Pilfer : Module {
 	}
 
 	void onPortChange(const PortChangeEvent& e) override{
+
 		if(inputs[AUDIO_Y_INPUT].isConnected()){
 			mode = 2;
+			lights[D1_LIGHT].setBrightness(0.f);
+			lights[D2_LIGHT].setBrightness(1.f);
 			return;
 		}
 		if(inputs[AUDIO_X_INPUT].isConnected()||inputs[SHAKE_CV_INPUT].isConnected()){
 			mode = 1;
+			lights[D1_LIGHT].setBrightness(1.f);
+			lights[D2_LIGHT].setBrightness(0.f);
 			return;
 		}
 		mode = 0;
+		lights[D1_LIGHT].setBrightness(0.f);
+		lights[D2_LIGHT].setBrightness(0.f);
 	}
 };
 
@@ -252,42 +340,47 @@ struct PilferWidget : ModuleWidget {
 		setModule(module);
 		setPanel(createPanel(asset::plugin(pluginInstance, "res/qd-002/Pilfer.svg")));
 
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(10.156, 10.421)), module, Pilfer::ACCEL_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(10.156, 20)), module, Pilfer::DRIVE_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(22.709, 10.628)), module, Pilfer::FRICT_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(49.871, 10.357)), module, Pilfer::TOLERANCE_PARAM));
-		addParam(createParamCentered<CKSS>(mm2px(Vec(49.871, 20)), module, Pilfer::HFLF_BUTTON));
-		addParam(createParamCentered<CKSS>(mm2px(Vec(49.871, 30)), module, Pilfer::LINEXP_BUTTON));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(35.73, 10.683)), module, Pilfer::BOUNCE_PARAM));
+		addParam(createParamCentered<RoundBigBlackKnob>(mm2px(Vec(16.51, 29.50)), module, Pilfer::ACCEL_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(38.735, 50.50)), module, Pilfer::DRIVE_PARAM));
+		addParam(createParamCentered<RoundBigBlackKnob>(mm2px(Vec(54.60, 29.50)), module, Pilfer::FRICT_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(18.46, 56.50)), module, Pilfer::TOLERANCE_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(52.66, 56.50)), module, Pilfer::BOUNCE_PARAM));
 
-		addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(10.456, 35.284)), module, Pilfer::SHAKE_CV_PARAM));
-		addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(10.456, 46.284)), module, Pilfer::ACC_CV_PARAM));
-		addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(23.009, 46.491)), module, Pilfer::FRI_CV_PARAM));
-		addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(50.171, 46.22)), module, Pilfer::TOL_CV_PARAM));
-		addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(50.03, 60.546)), module, Pilfer::DRI_CV_PARAM));
-		addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(36.03, 46.546)), module, Pilfer::BOU_CV_PARAM));
+		addParam(createParamCentered<CKSS>(mm2px(Vec(29.67, 18.00)), module, Pilfer::HFLF_BUTTON));
+		addParam(createParamCentered<CKSS>(mm2px(Vec(41.45, 18.00)), module, Pilfer::LINEXP_BUTTON));
+		addParam(createParamCentered<CKSS>(mm2px(Vec(35.56, 35.5)), module, Pilfer::RECTPOL_BUTTON));
 
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(10.412, 80.276)), module, Pilfer::ACC_CV_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(22.965, 80.483)), module, Pilfer::FRI_CV_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(50.127, 80.212)), module, Pilfer::TOL_CV_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(35.985, 80.538)), module, Pilfer::BOU_CV_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(35.73, 20.683)), module, Pilfer::DRI_CV_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(22.709, 20.628)), module, Pilfer::SHAKE_CV_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(16.789, 108.847)), module, Pilfer::AUDIO_X_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(16.789, 118.847)), module, Pilfer::AUDIO_Y_INPUT));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(35.56, 116.50)), module, Pilfer::SHAKE_CV_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(10.16, 76.00)), module, Pilfer::ACC_CV_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(60.96, 76.00)), module, Pilfer::FRI_CV_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(22.86, 76.00)), module, Pilfer::TOL_CV_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(35.56, 76.00)), module, Pilfer::DRI_CV_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(48.26, 76.00)), module, Pilfer::BOU_CV_PARAM));
 
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(29.291, 109.069)), module, Pilfer::AUDIO_X_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(29.291, 119.069)), module, Pilfer::AUDIO_Y_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(40, 115.069)), module, Pilfer::VEL_OUTPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(10.16, 93.00)), module, Pilfer::ACC_CV_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(60.96, 93.00)), module, Pilfer::FRI_CV_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(22.86, 91.00)), module, Pilfer::TOL_CV_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(48.26, 91.00)), module, Pilfer::BOU_CV_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(35.56, 90.00)), module, Pilfer::DRI_CV_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(35.56, 102.5)), module, Pilfer::SHAKE_CV_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(10.16, 111.00)), module, Pilfer::AUDIO_X_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(22.86, 111.00)), module, Pilfer::AUDIO_Y_INPUT));
 
-		addChild(createLightCentered<MediumSimpleLight<WhiteLight>>(mm2px(Vec(53.866, 97.021)), module, Pilfer::VELOCITY_LIGHT));
-		addChild(createLightCentered<MediumSimpleLight<WhiteLight>>(mm2px(Vec(54.19, 105.769)), module, Pilfer::BOUNCING_LIGHT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(48.26, 111.00)), module, Pilfer::AUDIO_X_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(60.96, 111.00)), module, Pilfer::AUDIO_Y_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(54.40, 101.80)), module, Pilfer::VEL_OUTPUT));
+
+		addChild(createLightCentered<SmallSimpleLight<RedLight>>(mm2px(Vec(35.56, 15.75)), module, Pilfer::VELOCITY_LIGHT));
+		addChild(createLightCentered<SmallSimpleLight<RedLight>>(mm2px(Vec(35.56, 20.25)), module, Pilfer::BOUNCING_LIGHT));
+		addChild(createLightCentered<SmallSimpleLight<BlueLight>>(mm2px(Vec(22.83, 102.50)), module, Pilfer::D1_LIGHT));
+		addChild(createLightCentered<SmallSimpleLight<YellowLight>>(mm2px(Vec(27.96, 105.00)), module, Pilfer::D2_LIGHT));
 	}
 	void appendContextMenu(Menu* menu) override {
 		Pilfer* module = getModule<Pilfer>();
 
 		menu->addChild(new MenuSeparator);
-		menu->addChild(createMenuItem("Mode",std::to_string(module->mode)+"d"));
+
+		//add theme here
 	}
 };
 
